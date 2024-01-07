4433739ce9d82b879af61483cd2a9403be569e98
xxternwj <ext.joakim.a.ternvald@tietoevry.com>
2024-01-07 18:15:52 +0100

    Added test project

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace TestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void EnsureTrue()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
